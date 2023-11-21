07b8f4f54f369a29bdc13cd03ee0aa06ad2c9dd5
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-21 14:56:49 +0300

    MySQL setup development

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,6 @@
 -- Script that creates required database
-CREATE DATABASE IF NOT EXISTS hbtn_test_db;
-CREATE USER IF ;
-GRANT 
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT ALL ON hbnb_dev_db TO hbnb_dev;
+GRANT SELECT ON performance_schema TO ;
